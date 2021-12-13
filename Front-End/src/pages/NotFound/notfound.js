dbba81791cd5309b43c47054b591e1307c8f4581
Hugo Ramoz Andrade <hugoramoz37@gmail.com>
2021-12-13 09:57:17 -0300

    Add files via upload

---
+++ Front-End/src/pages/NotFound/notfound.js
@@ -0,0 +1,16 @@
+import './notfound.css'
+import image from '../../assets/3828537.jpg'
+import wave from '../../assets/wave-not.svg'
+
+export default function notFound(){
+    return(
+        <div className="divBody">
+             <img className="wave" src={wave} />
+            <div className="container">
+           
+                <h2> 404 || NOTFOUND </h2>
+                <img className="img-404" src={image} alt="Not found" />
+            </div>
+        </div>
+    );
+}

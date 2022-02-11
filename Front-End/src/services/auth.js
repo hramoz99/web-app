a6a2933d2f575a69145a7c05c27002f8c2df1409
Hugo Ramoz Andrade <hugoramoz37@gmail.com>
2022-02-11 17:51:16 -0300

    Delete Front-End directory

--- Front-End/src/services/auth.js
+++
@@ -1,7 +0,0 @@
-export const parseJwt = () => {
-    let base64 = localStorage.getItem('usuario-login').split('.')[1]
-
-    return JSON.parse(window.atob(base64))
-}
-
-export const usuarioAutenticado = () => localStorage.getItem('usuario-login') !== null;
